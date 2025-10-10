1968573adf4e18eae61db54dcfb3646dca5faf47
Слушатель <@CBRMINIPC-DT002>
2025-10-10 12:32:07 +0300

    Добавили лабу1

---
+++ Laba2/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите арифметическое выражение с пробелами между числами и оператором():");
+                    string exp = "";
+                    exp = Console.ReadLine();
+                    string[] dig = exp.Split(' ');
+                    double dig1 = Convert.ToDouble(dig[0]);
+                    double dig2 = Convert.ToDouble(dig[2]);
+                    string operand = dig[1];
+                    double ans = 0;
+
+                    switch (operand)
+                    {
+                        case "+":
+                            ans = dig1 + dig2;
+                            break;
+                        case "-":
+                            ans = dig1 - dig2;
+                            break;
+                        case "*":
+                            ans = dig1 * dig2;
+                            break;
+                        case "/":
+                            ans = dig1 / dig2;
+                            break;
+                        case "^":
+                            ans = Math.Pow(dig1, dig2);
+                            break;
+                        case "sqrt":
+                            ans = Math.Pow(dig1, 1 / dig2);
+                            break;
+                        case "log":
+                            ans = Math.Log(dig1, dig2);
+                            break;
+                        case "ln":
+                            ans = Math.Log(dig1);
+                            break;
+                    }
+                    Console.WriteLine("\n Ответ: " + exp + " = " + Convert.ToString(ans));
+                }
+                catch
+                {
+                    Console.WriteLine("Введен неправильный оператор или число!");
+                }
+            }
+        }
+
+    }
+
+}
+
